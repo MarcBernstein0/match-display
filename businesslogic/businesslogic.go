5b7b24be5576b53224ab3458fd00fc904bfd0df8
Marc Bernstein <marcbernstein98@gmail.com>
2021-11-11 14:12:13 -0500

    start building getting the list of participants from all the running tournaments
        -- built unit tests for getting list of all tournaments

--- businesslogic/businesslogic.go
+++ businesslogic/businesslogic.go
@@ -23,7 +23,7 @@ type HTTPClient interface {
 	Do(req *http.Request) (*http.Response, error)
 }
 
-const API_URL = "https://api.challonge.com/v1/tournaments.json"
+const API_URL = "https://api.challonge.com/v1"
 
 func init() {
 	// Open config file
@@ -39,6 +39,31 @@ func init() {
 	client = &http.Client{}
 }
 
+/* Generic function that builds and http request. Only builds get requests as no post calls will happen
+args:
+	apiPath string path of the api call
+	params map[string]string all the parameters that will be passed into the
+	                             request where key is the parameter and value is the parameter value
+return:
+	*http.Request the fully built request ready to be sent
+	error errors that occur when building the request
+*/
+func httpQueryBuilder(apiPath string, params map[string]string) (*http.Request, error) {
+	fullAPIPath := fmt.Sprintf("%s/%s.json", API_URL, apiPath)
+	req, err := http.NewRequest("GET", fullAPIPath, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request.\n%v", err)
+	}
+	// build query
+	q := req.URL.Query()
+	q.Add("api_key", config.ApiKey)
+	for k, v := range params {
+		q.Add(k, v)
+	}
+	req.URL.RawQuery = q.Encode()
+	return req, nil
+}
+
 /* calls challenonge api to get all running tournaments
    created recently
    args:
@@ -52,17 +77,16 @@ func getTournaments(client HTTPClient) (map[int]string, error) {
 	// map of tournamentIDs and game names
 	tournaments := make(map[int]string)
 
-	// create request to client
-	req, err := http.NewRequest("GET", API_URL, nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create request.\n%v", err)
+	// parameters to pass in
+	params := map[string]string{
+		"state": "in_progress",
 	}
 
-	// add request parameters
-	q := req.URL.Query()
-	q.Add("api_key", config.ApiKey)
-	q.Add("state", "in_progress")
-	req.URL.RawQuery = q.Encode()
+	// create request to client
+	req, err := httpQueryBuilder("tournaments", params)
+	if err != nil {
+		return nil, fmt.Errorf("req failed in getTouranments call\n. %v", err)
+	}
 
 	// call api client and handle response
 	res, err := client.Do(req)
@@ -90,6 +114,16 @@ func getTournaments(client HTTPClient) (map[int]string, error) {
 	}
 
 	return tournaments, nil
+}
+
+func getParticipants(tournaments map[int]string, client HTTPClient) (map[int]string, error) {
+	apiPath := fmt.Sprintf("tournaments/%s/participants", "10469768")
+	req, err := httpQueryBuilder(apiPath, nil)
+	if err != nil {
+		return nil, fmt.Errorf("req failed in getTouranments call\n. %v", err)
+	}
+	fmt.Println(req.URL.String())
+	return nil, nil
 }
 
 func TestIntegration() {
--- businesslogic/businesslogic_test.go
+++ businesslogic/businesslogic_test.go
@@ -41,3 +41,35 @@ func TestGetTournaments(t *testing.T) {
 		t.Fatalf("Tournamet list did not come back the same. Expected=%v, got=%v\n", expectedResult, mapResult)
 	}
 }
+
+func TestGetParticipants(t *testing.T) {
+	mockTournaments := map[int]string{
+		10469768: "Melty Blood: Type Lumina",
+	}
+	expectedResult := map[int]string{
+		158464100: "Marc",
+		158464107: "KosherSalt",
+		158464116: "Bernstein",
+		158464118: "Test",
+		158464119: "Test2",
+		158464121: "Test3",
+		158464124: "Test4",
+	}
+	testData, err := os.ReadFile("./test-data/testParticipantsData.json")
+	if err != nil {
+		t.Errorf("Failed to read the test file\n%v\n", err)
+	}
+	mockClient.DoFunc = func(req *http.Request) (*http.Response, error) {
+		return &http.Response{
+			StatusCode: 200,
+			Body:       io.NopCloser(bytes.NewReader(testData)),
+		}, nil
+	}
+	participantsResults, err := getParticipants(mockTournaments, mockClient)
+	if err != nil {
+		t.Errorf("getParticipants failed\n%v\n", err)
+	}
+	if !reflect.DeepEqual(expectedResult, participantsResults) {
+		t.Fatalf("Participants list did not come back the same. Expected=%v, got=%v\n", expectedResult, participantsResults)
+	}
+}
