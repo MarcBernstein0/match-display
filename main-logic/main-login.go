e67ac54ba6e065f8970cd5465dee199199dfdba2
Marc Bernstein <marcbernstein98@gmail.com>
2022-05-08 22:40:14 -0400

    add unit test to get tournament info
        -- add unit test for tournament info
        -- add functionality to get tournament info
        -- add method for getting participants to Fetch interface

--- main-logic/main-login.go
+++ main-logic/main-login.go
@@ -2,11 +2,35 @@ package mainlogic
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 	"time"
 )
 
+const API_URL = "https://api.challonge.com/v1"
+
+var configuration config
+
 type (
+	Tournaments struct {
+		Tournament struct {
+			Id   int    `json:"id"`
+			Name string `json:"name"`
+		} `json:"tournament"`
+	}
+	Participants struct {
+		Participant struct {
+			Id           int    `json:"id"`
+			TournamentId int    `json:"tournament_id"`
+			Name         string `json:"name"`
+		} `json:"participant"`
+	}
+	config struct {
+		username string
+		apiKey   string
+	}
 	client struct {
 		baseURL string
 		client  *http.Client
@@ -14,9 +38,15 @@ type (
 	}
 
 	Fetch interface {
-		FetchData(ctx context.Context, urlPath string, params map[string]string) (string, error)
+		FetchTournamentData(ctx context.Context, date string) ([]Tournaments, error)
+		FetchParticipantData(ctx context.Context) ([]Participants, error)
 	}
 )
+
+func init() {
+	configuration.username = os.Getenv("USER_NAME")
+	configuration.apiKey = os.Getenv("API_KEY")
+}
 
 func New(baseURL string, httpClient *http.Client, timeout time.Duration) *client {
 	return &client{
@@ -26,6 +56,34 @@ func New(baseURL string, httpClient *http.Client, timeout time.Duration) *client
 	}
 }
 
-func (c *client) FetchData(ctx context.Context, urlPath string, params map[string]string) (string, error) {
-	return "", nil
+func (c *client) FetchTournamentData(ctx context.Context, date string) ([]Tournaments, error) {
+	// get tournament
+	var tournaments []Tournaments
+
+	ctx, cancel := context.WithTimeout(ctx, c.timeout)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, c.baseURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	q := req.URL.Query()
+	q.Add("api_key", configuration.apiKey)
+	q.Add("state", "in_progress")
+	fmt.Println(date)
+	q.Add("created_after", date)
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	json.NewDecoder(res.Body).Decode(&tournaments)
+
+	return tournaments, nil
+}
+
+func (c *client) FetchParticipantData(ctx context.Context) ([]Participants, error) {
+	return nil, nil
 }
